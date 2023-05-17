e7fc90d2a0de7607c5210c8a20228b4c4e814818
Loc Tran <ducloc0009@gmail.com>
2023-05-17 22:52:20 +0700

    feat: Add user personal posts info endpoints

--- main/controllers/user.py
+++ main/controllers/user.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, jsonify, request
-from ..engines.user import get_user_by_id
-from ..schemas.user import UserResponseSchema
-from ..common.decorators import require_access_token
-from ..libs.common import to_response
+from flask import Blueprint
+from main.engines.user import get_user_by_id
+from main.schemas.user import UserResponseSchema
+from main.schemas.post import PostResponseSchema
+from main.libs.common import to_response
 
 userController = Blueprint("user", __name__)
 
@@ -15,3 +15,45 @@ def get_user_with_id(id):
         return {"message": "User not found"}, 404
 
     return {"user": to_response(schema=UserResponseSchema, clsObject=user)}, 200
+
+
+@userController.get("/users/<id>/liked")
+def get_user_liked_posts_by_id(id):
+    user = get_user_by_id(id)
+
+    if user is None:
+        return {"message": "User not found"}, 404
+
+    response = []
+    for post in user.likes:
+        response.append(to_response(schema=PostResponseSchema, clsObject=post))
+
+    return {"total_item": len(response), "items": response}, 200
+
+
+@userController.get("/users/<id>/read")
+def get_user_liked_posts_by_id(id):
+    user = get_user_by_id(id)
+
+    if user is None:
+        return {"message": "User not found"}, 404
+
+    response = []
+    for post in user.reads:
+        response.append(to_response(schema=PostResponseSchema, clsObject=post))
+
+    return {"total_item": len(response), "items": response}, 200
+
+
+@userController.get("/users/<id>/favorites")
+def get_user_liked_posts_by_id(id):
+    user = get_user_by_id(id)
+
+    if user is None:
+        return {"message": "User not found"}, 404
+
+    response = []
+    for post in user.favorites:
+        response.append(to_response(schema=PostResponseSchema, clsObject=post))
+
+    return {"total_item": len(response), "items": response}, 200
